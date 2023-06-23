ab38e883d9c09536a175ff645e9b38e1e0d46291
Fox Reymann <foxreymann@users.noreply.github.com>
2023-06-23 07:47:54 +0100

    handle events with proper names, messages, final clean up (#11)
    Co-authored-by: foxreymann <fox>

--- backend/src/writers.ts
+++ backend/src/writers.ts
@@ -6,67 +6,35 @@ import { Telegraf } from 'telegraf';
 import { LocalStorage } from 'node-localstorage';
 const keyStore = new LocalStorage('./scratch');
 
-export async function handleDeploy() {
-  // Run logic as at the time Contract was deployed.
+export async function handleEscapeGuardianTriggered({ block, tx, rawEvent, mysql }: Parameters<CheckpointWriter>[0]) {
+  if (!rawEvent) return
+  handleAnything({
+    eventName: 'Escape Guardian Triggered',
+    block,
+    tx,
+    rawEvent,
+    mysql
+  })
 }
 
-export async function handleAnything({ block, tx, rawEvent, mysql }: Parameters<CheckpointWriter>[0]) {
-console.log('HANDLE ANYTHING')
-//  if (!event) return;
-console.log('EVENT')
-console.log({rawEvent})
-console.log('send message')
-  let wallet = rawEvent && toAddress(rawEvent.from_address);
+export async function handleGuardianChanged({ block, tx, rawEvent, mysql }: Parameters<CheckpointWriter>[0]) {
+  if (!rawEvent) return
+  handleAnything({
+    eventName: 'Guardian Changed',
+    block,
+    tx,
+    rawEvent,
+    mysql
+  })
+
+}
+
+export async function handleAnything({ eventName, block, tx, rawEvent, mysql }) {
+  if (!rawEvent) return;
+  let wallet = toAddress(rawEvent.from_address);
   wallet = getChecksumAddress(wallet) || true
-  const msg = 'Recovery'
+  const msg = `${eventName} event for ${wallet}`
   sendTelegramMsg(wallet, msg)
-}
-
-// This decodes the new_post events data and stores successfully
-// decoded information in the `posts` table.
-//
-// See here for the original logic used to create post transactions:
-// https://gist.github.com/perfectmak/417a4dab69243c517654195edf100ef9#file-index-ts
-export async function handleNewPost({ block, tx, event, mysql }: Parameters<CheckpointWriter>[0]) {
-  if (!event) return;
-
-  const author = toAddress(event.data[0]);
-  let content = '';
-  let tag = '';
-  const contentLength = BigInt(event.data[1]);
-  const tagLength = BigInt(event.data[2 + Number(contentLength)]);
-  const timestamp = block && block.timestamp;
-  const blockNumber = block && block.block_number;
-
-  // parse content bytes
-  try {
-    content = hexStrArrToStr(event.data, 2, contentLength);
-  } catch (e) {
-    console.error(`failed to decode content on block [${blockNumber}]: ${e}`);
-    return;
-  }
-
-  // parse tag bytes
-  try {
-    tag = hexStrArrToStr(event.data, 3 + Number(contentLength), tagLength);
-  } catch (e) {
-    console.error(`failed to decode tag on block [${blockNumber}]: ${e}`);
-    return;
-  }
-
-  // post object matches fields of Post type in schema.gql
-  const post = {
-    id: `${author}/${tx.transaction_hash}`,
-    author,
-    content,
-    tag,
-    tx_hash: tx.transaction_hash,
-    created_at: timestamp,
-    created_at_block: blockNumber
-  };
-
-  // table names are `lowercase(TypeName)s` and can be interacted with sql
-  await mysql.queryAsync('INSERT IGNORE INTO posts SET ?', [post]);
 }
 
 function sendTelegramMsg(address: string, msg: string) {
